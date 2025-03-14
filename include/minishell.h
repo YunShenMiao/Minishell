92879dd64b73ea80a70b2e13db7fe8589c13a300
yxuefei <yxuefeiluvbunny@gmail.com>
2025-03-14 21:09:19 +0100

    add some lib in header

--- include/minishell.h
+++ include/minishell.h
@@ -2,9 +2,14 @@
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <fcntl.h>
 
 typedef struct s_memory_track
 {
