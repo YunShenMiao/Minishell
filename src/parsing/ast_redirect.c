c53eb287c71757bcf2d6f7d4c35a28aba4abccfe
Joana Claire Elinor Wardenga <jwardeng@2-A-1.42heilbronn.de>
2025-05-07 18:15:06 +0200

    added cmd_node for empty cmd (>> out)

--- src/parsing/ast_redirect.c
+++ src/parsing/ast_redirect.c
@@ -6,11 +6,32 @@
 /*   By: jwardeng <jwardeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 14:41:12 by jwardeng          #+#    #+#             */
-/*   Updated: 2025/05/07 14:59:41 by jwardeng         ###   ########.fr       */
+/*   Updated: 2025/05/07 18:13:42 by jwardeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+t_ast *add_cmd(t_token_data *token_data, t_tok_type type)
+{
+	t_ast *cmd_node;
+
+	if (type == TOK_APPEND || type == TOK_REDIRECT_OUT)
+		{
+		cmd_node = create_ast_node(&token_data, TOK_COMMAND);
+		cmd_node->args = (char**)gc_malloc(token_data->gc, PARSING, sizeof(char**));
+		cmd_node->args[0] = ft_env_strdup("echo-n", token_data->gc);
+		cmd_node->args[1] = NULL;
+		}
+	else
+		{
+		cmd_node = create_ast_node(&token_data, TOK_COMMAND);
+		cmd_node->args = (char**)gc_malloc(token_data->gc, PARSING, sizeof(char**));
+		cmd_node->args[0] = ft_env_strdup("cat", token_data->gc);
+		cmd_node->args[1] = NULL;
+		}
+	return(cmd_node);
+}
 
 t_ast	*parse_append(t_token_data **token_data, t_token **current, t_ast *prev)
 {
@@ -27,12 +48,14 @@ t_ast	*parse_append(t_token_data **token_data, t_token **current, t_ast *prev)
 		return ((*token_data)->syntax_error = 1, NULL);
 	}
 	re_node = create_ast_node(token_data, TOK_APPEND);
-	re_node->type = TOK_APPEND;
 	file_node = create_ast_node(token_data, TOK_FILE);
 	file_node->file_name = ft_strndup((*token_data)->gc, (*current)->value, 0,
 			ft_strlen((*current)->value));
 	re_node->right = file_node;
 	*current = (*current)->next;
+	if (!prev)
+	re_node->left = add_cmd(*token_data, TOK_APPEND);
+	else
 	re_node->left = prev;
 	return (re_node);
 }
@@ -53,7 +76,6 @@ t_ast	*parse_heredoc(t_token_data **token_data, t_token **current,
 		return ((*token_data)->syntax_error = 1, NULL);
 	}
 	re_node = create_ast_node(token_data, TOK_HEREDOC);
-	re_node->type = TOK_HEREDOC;
 	file_node = create_ast_node(token_data, TOK_FILE);
 	file_node->file_name = ft_strndup((*token_data)->gc, (*current)->value, 0,
 			ft_strlen((*current)->value));
@@ -79,12 +101,14 @@ t_ast	*parse_re_out(t_token_data **token_data, t_token **current, t_ast *prev)
 		return ((*token_data)->syntax_error = 1, NULL);
 	}
 	re_node = create_ast_node(token_data, TOK_REDIRECT_OUT);
-	re_node->type = TOK_REDIRECT_OUT;
 	file_node = create_ast_node(token_data, TOK_FILE);
 	file_node->file_name = ft_strndup((*token_data)->gc, (*current)->value, 0,
 			ft_strlen((*current)->value));
 	re_node->right = file_node;
 	*current = (*current)->next;
+	if (!prev)
+	re_node->left = add_cmd(*token_data, TOK_REDIRECT_OUT);
+	else
 	re_node->left = prev;
 	return (re_node);
 }
@@ -104,15 +128,14 @@ t_ast	*parse_re_in(t_token_data **token_data, t_token **current, t_ast *prev)
 		return ((*token_data)->syntax_error = 1, NULL);
 	}
 	re_node = create_ast_node(token_data, TOK_REDIRECT_IN);
-	re_node->type = TOK_REDIRECT_IN;
 	file_node = create_ast_node(token_data, TOK_FILE);
 	file_node->file_name = ft_strndup((*token_data)->gc, (*current)->value, 0,
 			ft_strlen((*current)->value));
 	re_node->left = file_node;
 	*current = (*current)->next;
-	// if ((*current)->type == TOK_WORD)
-	// re_node->right = create_ast_node(token_data, TOK_COMMAND);
-	// else
+	if (!prev)
+	re_node->right = add_cmd(*token_data, TOK_REDIRECT_IN);
+	else
 	re_node->right = prev;
 	return (re_node);
 }
@@ -121,9 +144,8 @@ t_ast	*parse_redirections(t_token_data **token_data, t_token **current,
 		t_ast *prev)
 {
 	t_ast	*redir;
-	// t_ast *cmd;
+	
 	redir = NULL;
-	// cmd = NULL;
 	while (*current && ((*current)->type == TOK_REDIRECT_IN
 			|| (*current)->type == TOK_REDIRECT_OUT
 			|| (*current)->type == TOK_APPEND
@@ -137,12 +159,6 @@ t_ast	*parse_redirections(t_token_data **token_data, t_token **current,
 			redir = (parse_append(token_data, current, prev));
 		else if ((*current)->type == TOK_HEREDOC)
 			redir = (parse_heredoc(token_data, current, prev));
-		// else if ((*current)->type == TOK_WORD)
-		// {
-		// 	cmd = create_ast_node(token_data, TOK_COMMAND);
-		// 	parse_command_args(token_data, current,cmd);
-		// 	redir->left = cmd;
-		// }
 		prev = redir;
 	}
 	return (redir);
